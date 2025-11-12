7f912bd89c2a3c6c7b91d087bd96c2746b3fc6cb
JanNiklasBlankenhorn <jan.blankenhorn@alumni.fh-aachen.de>
2025-11-12 14:50:57 +0100

    Meine änderungen

---
+++ Lernwelt/src/lib/supabaseClient.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+
+// Importiert die Keys sicher aus deiner .env-Datei (SvelteKit-Standard)
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
+
+// Erstellt den Client
+export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY
